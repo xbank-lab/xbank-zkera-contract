bbdd1c1981ed41b14a0cbf312e2a0d27dd7e652c
eddiexbank <eddie.xbank@gmail.com>
2023-05-03 17:38:17 +0700

    build: separate deployment scripts

--- deploy/deploy_priceOracle.ts
+++ deploy/deploy_priceOracle.ts
@@ -2,19 +2,20 @@ import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
 import { config as dotEnvConfig } from "dotenv";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { Wallet } from "zksync-web3";
-import { deployMulticall } from "./utils/deploy";
+import { deploySimplePriceOracle } from "./utils/deploy";
 
 dotEnvConfig();
 
 // REVIEW VARIABLES CAREFULLY
 const deployerWallet = new Wallet(process.env.DEPLOYER_PK as string);
 
+// An example of a deploy script that will deploy and call a simple contract.
 export default async function (hre: HardhatRuntimeEnvironment) {
   // Create deployer object and load the artifact of the contract we want to deploy.
   const deployer = new Deployer(hre, deployerWallet);
   console.log("# Deployer address:", deployer.zkWallet.address);
 
-  // deploy multicall
-  const multicall = await deployMulticall(deployer);
-  console.log(`# Multicall deployed at: ${multicall.address}`);
+  // deploy price oracle
+  const simplePriceOracle = await deploySimplePriceOracle(deployer);
+  console.log(`# PriceOracle deployed at: ${simplePriceOracle.address}`);
 }
